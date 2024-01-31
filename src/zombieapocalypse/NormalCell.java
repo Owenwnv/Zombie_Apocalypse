0ef210b1c563f29a356e47348919cb8df4202e70
Owen Wanaverbecque <owen.wanaverbecque.etu@univ-lille.fr>
2024-01-31 11:31:09 +0100

    normalcell class

---
+++ src/zombieapocalypse/NormalCell.java
@@ -0,0 +1,17 @@
+package zombieapocalypse;
+public class NormalCell implements Cell{
+
+    private String name;
+
+    public NormalCell() {
+        this.name = "Normal";
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String toString() {
+        return "This is a NormalCell";
+    }
+}
