cebc51da168e3a2d645bfb61e89b7d42a9064c32
farzadde.soleimanian.etu <farzadde.soleimanian.etu@univ-lille.fr>
2024-02-03 17:50:08 +0100

    documentation de cell

--- src/zombieapocalypse/Cell.java
+++ src/zombieapocalypse/Cell.java
@@ -1,5 +1,16 @@
 package zombieapocalypse;
+
+/* Class representing a cell */
 public interface Cell {
+    /**
+     * Returns this Cell's name
+     * @return Cell's name
+     */
     public String getName();
+
+    /**
+     * Returns a description of this Cell 
+     * @return description of this Cell 
+     */
     public String toString();
 }
