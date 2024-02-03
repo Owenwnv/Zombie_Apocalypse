9bb72081ea467ebe462f08b83e43a4870767d32b
farzadde.soleimanian.etu <farzadde.soleimanian.etu@univ-lille.fr>
2024-02-03 19:00:55 +0100

    documentation de  map

--- src/zombieapocalypse/Map.java
+++ src/zombieapocalypse/Map.java
@@ -1,10 +1,19 @@
 package zombieapocalypse;
 
+/* Class representing a map */
 public class Map {
+    // height of this Map
     private int height;
+    // width of this Map
     private int width;
+    // Cells of this Map
     private Cell[][] cells;
 
+    /**
+     * Builds a Map
+     * @param height height of this Map
+     * @param width width of this Map
+     */
     public Map(int height, int width) {
         this.height = height;
         this.width = width;
@@ -15,18 +24,37 @@ public class Map {
         }
     }
 
+    /**
+     * Returns this Map's height
+     * @return Map's height
+     */
     public int getHeight() {
         return this.height;
     }
 
+    /**
+     * Returns this Map's width
+     * @return Map's width
+     */
     public int getWidth() {
         return this.width;
     }
 
+    /**
+     * Returns specified Cell of this Map
+     * @param x x coordinate of the Cell
+     * @param y y coordinate of the Cell
+     */
     public Cell getCell(int x, int y) {
         return this.cells[x][y];
     }
 
+    /**
+     * Sets specified Cell of this Map
+     * @param x x coordinate of the Cell
+     * @param y y coordinate of the Cell
+     * @param cell cell to replace with
+     */
     public void setCell(int x, int y, Cell cell) {
         this.cells[x][y] = cell;
     }
