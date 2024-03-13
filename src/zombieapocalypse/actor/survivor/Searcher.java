743a60b3d275352fc47b21aa4e35869fc591d796
Owen Wanaverbecque <owen.wanaverbecque.etu@univ-lille.fr>
2024-03-13 14:42:46 +0100

    Searcher class

---
+++ src/zombieapocalypse/actor/survivor/Searcher.java
@@ -0,0 +1,11 @@
+package zombieapocalypse.actor.survivor;
+
+public class Searcher extends Survivor {
+
+    /**
+     * Searcher's feature
+     */
+    public Searcher() {
+        super();
+    }
+}
