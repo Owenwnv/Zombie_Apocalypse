7ba6f58baa055d3294363f31ed6fab80d283283e
Owen Wanaverbecque <owen.wanaverbecque.etu@univ-lille.fr>
2024-03-13 14:24:24 +0100

    Abomination class

---
+++ src/zombieapocalypse/actor/zombie/Abomination.java
@@ -0,0 +1,11 @@
+package zombieapocalypse.actor.zombie;
+
+public class Abomination extends Zombie {
+
+    /**
+     * Abomination's feature
+     */
+    public Abomination() {
+        super(3, 6);
+    }
+}
