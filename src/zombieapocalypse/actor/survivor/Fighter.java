969d629e4b9ec5d212b83e30a3d05bcc456653ec
Owen Wanaverbecque <owen.wanaverbecque.etu@univ-lille.fr>
2024-03-13 14:41:55 +0100

    Fighter class

---
+++ src/zombieapocalypse/actor/survivor/Fighter.java
@@ -0,0 +1,11 @@
+package zombieapocalypse.actor.survivor;
+
+public class Fighter extends Survivor {
+
+    /**
+     * Fighter's feature
+     */
+    public Fighter() {
+        super();
+    }
+}
