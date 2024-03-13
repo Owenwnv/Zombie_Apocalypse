f25e42ec0a4b9fd86142054cd635b2a050fddf73
Owen Wanaverbecque <owen.wanaverbecque.etu@univ-lille.fr>
2024-03-13 14:20:48 +0100

    Runner class

---
+++ src/zombieapocalypse/actor/zombie/Runner.java
@@ -0,0 +1,11 @@
+package zombieapocalypse.actor.zombie;
+
+public class Runner extends Zombie {
+
+    /**
+     * Runner's feature
+     */
+    public Runner() {
+        super(1, 2);
+    }
+}
--- src/zombieapocalypse/actor/zombie/Walker.java
+++ src/zombieapocalypse/actor/zombie/Walker.java
@@ -2,6 +2,9 @@ package zombieapocalypse.actor.zombie;
 
 public class Walker extends Zombie {
 
+    /**
+     * Walker's feature
+     */
     public Walker() {
         super(1, 1);
     }
