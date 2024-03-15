c4bcf47a6461f88f4b9ae057ef74df191e2d2ea4
farzadde.soleimanian.etu <farzadde.soleimanian.etu@univ-lille.fr>
2024-03-15 20:39:40 +0100

    format de Livrable2

--- src/zombieapocalypse/Livrable2.java
+++ src/zombieapocalypse/Livrable2.java
@@ -19,33 +19,41 @@ public class Livrable2 {
     public static void main(String[] args) {
         int width = 5;
         int height = 5;
+
         if (args.length > 1) {
             width = Integer.parseInt(args[0]);
             height = Integer.parseInt(args[1]);
         }
+
         MapGenerator mapg = new MapGenerator(width, height);
         Map map = mapg.generateMap();
         Game game = new Game(map);
+
         Survivor fighter = new Fighter();
         Survivor healer = new Healer();
         Survivor lucky = new Lucky();
         Survivor searcher = new Searcher();
+
         fighter.addItemToBackpack(new HandheldMap());
         healer.addItemToBackpack(new HandheldMap());
         lucky.addItemToBackpack(new HandheldMap());
         searcher.addItemToBackpack(new HandheldMap());
+
         fighter.putItemInHand(new HealthPotion());
         healer.putItemInHand(new HealthPotion());
         lucky.putItemInHand(new HealthPotion());
         searcher.putItemInHand(new HealthPotion());
+
         game.spawnSurvivor(fighter, 2, 2);
         game.spawnSurvivor(healer, 2, 2);
         game.spawnSurvivor(lucky, 2, 2);
         game.spawnSurvivor(searcher, 2, 2);
+
         game.spawnZombie(new Abomination(), 2, 2);
         game.spawnZombie(new Bigboy(), 0, 0);
         game.spawnZombie(new Runner(), 0, 2);
         game.spawnZombie(new Walker(), 3, 3);
+
         game.moveUpSurvivors();
         game.getMap().showMap();
     }
