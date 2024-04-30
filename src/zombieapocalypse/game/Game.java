b7787262a4955336629c5d99f1fdc63252dfc1fd
farzadde.soleimanian.etu <farzadde.soleimanian.etu@univ-lille.fr>
2024-04-30 19:08:50 +0200

    suppression de moveUpSurvivors

--- src/zombieapocalypse/game/Game.java
+++ src/zombieapocalypse/game/Game.java
@@ -159,26 +159,6 @@ public class Game {
     }
 
     /**
-     * Moves survivors up by one cell if possible.
-     */
-    public void moveUpSurvivors() {
-        for (int i = 0; i < this.map.getWidth(); i++) {
-            for (int j = 0; j < this.map.getHeight(); j++) {
-                Cell cell = this.map.getCell(i, j);
-                if (cell.getSurvivors().size() > 0 && i - 1 >= 0) {
-                    Cell topCell = this.map.getCell(i - 1, j);
-                    Iterator<Survivor> iterator = cell.getSurvivors().iterator();
-                    while (iterator.hasNext()) {
-                        Survivor survivor = iterator.next();
-                        topCell.addSurvivor(survivor);
-                        iterator.remove();
-                    }
-                }
-            }
-        }
-    }
-
-    /**
      * Returns the map of the game.
      * 
      * @return The map of the game
